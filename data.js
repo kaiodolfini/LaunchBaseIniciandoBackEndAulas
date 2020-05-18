f92e3c744e2c0364a90ccbcb3370ba50263cfecb
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-05-18 20:08:49 -0300

    Adicionando Dados Dinâmicos

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "RJyziqiTRcA",
+        title: "Pilates em casa - Garrafa Pet",
+        duration: "7 min",
+        price: "Free"
+    },
+    {
+        id: "EkgP0DNaa7E",
+        title: "Pilates em casa - Com Meias",
+        duration: "3 min 14seg",
+        price: "Free"
+    },
+    {
+        id: "s2w8X84GPyY",
+        title: "Pilates em Casa - Alongamento",
+        duration: "7 min 02seg",
+        price: "Free"
+    },
+    {
+        id: "mP1w0YezfLo",
+        title: "Pilates em Casa - Utilizando uma Toalha",
+        duration: "6 min 48seg",
+        price: "Free"
+    },
+    {
+        id: "_qv8Zlw2lTY",
+        title: "Pilates em casa 2",
+        duration: "3 min 39seg",
+        price: "Free"
+    },
+    {
+        id: "csxUJvcvZQo",
+        title: "Pilates em Casa",
+        duration: "6 min 49seg",
+        price: "Free"
+    }
+]
--- server.js
+++ server.js
@@ -2,23 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
-server.get("/", function(req, res){
-    return res.render("sobre")
+server.get("/", function (req, res) {
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/55322321?s=460&u=33a029e8ed4965b0ebfc4b1325f8bbfeb31a92ca&v=4",
+        name: "Kaio Dolfini",
+        title: "Analista de Testes - Pamcary",
+        description: 'Analista de testes buscando conhecimento em desenvolvimento para mudar de área, fazendo o curso Launch Base da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "Github", url: "https://github.com/kaiodolfini/" },
+            { name:"LinkedIn", url:"https://www.linkedin.com/in/kaiodesouza/" },
+            { name: "Facebook", url: "https://www.facebook.com/kaio.dolfini" }
+        ]
+    }
+
+    return res.render("sobre", { about })
 })
 
-server.get("/classes", function(req, res){
-    return res.render("classes")
+server.get("/classes", function (req, res) {
+    return res.render("classes", { items: videos })
 })
 
-server.listen(5000, function(){
+server.listen(5000, function () {
     console.log("Server is Running")
 })
