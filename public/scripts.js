4e09c174127e66a3f229410cdabba35b3d51de7c
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-05-19 18:59:16 -0300

    Correção de identação

--- public/scripts.js
+++ public/scripts.js
@@ -5,6 +5,5 @@ for (let card of cards) {
     card.addEventListener("click", function () {
         const videoId = card.getAttribute("id");
         window.location.href = `/video?id=${videoId}`
-
     })
 }
